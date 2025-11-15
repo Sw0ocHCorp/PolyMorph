2dbcabe500f92d0fd4b9a4ac36740138fdd09ddd
Sw0ocHCorp <sw0ochcorp@gmail.com>
2025-11-15 22:09:26 +0100

    Setup project

--- Simulation/robomorph/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use godot::init::*;  // or godot::prelude::* depending on your version
-
-struct MyExtension;
-
-#[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {
-    // optionally override methods like on_level_init, etc.
-}
