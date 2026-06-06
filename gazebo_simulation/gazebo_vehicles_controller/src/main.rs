14ac918ff3a7561d76bf3613cfbbe812dadd58ea
Sw0ocHCorp <sw0ochcorp@gmail.com>
2026-06-06 12:23:17 +0200

    Setup Gazebo environment OK
    -   Vehicles: Folder that contains the vehicle models files
        +   Osprai:
             *   Bicopter drone with
                     2 Arms
                     2 Propellers
                     1 Lidar
                     1 GNSS
                     1 Imu
                     2 cameras for stereovision
    -   gazebo_vehicles_controller: Rust project folder used to build the vehicle controllers
        +   Basic controller for the osprai vehicle that able to publish / subscribe to the topics

---
+++ gazebo_simulation/gazebo_vehicles_controller/src/main.rs
@@ -0,0 +1,274 @@
+use std::{ops::Add, sync::{Arc, Mutex}, thread::sleep, time::{Duration, Instant}, u8};
+
+use gz::{msgs::{actuators::Actuators, double::Double, imu::IMU, laserscan::LaserScan, model::Model, navsat::NavSat}, transport::Node};
+
+struct LidarState {
+    min_vertical_angle: f64,
+    vertical_resolution: f64,
+    min_horizontal_angle: f64,
+    horizontal_resolution: f64,
+    measurements: Vec<f64>,
+}
+
+#[derive(Clone)]
+struct ServoMotorState {
+    id: u32,
+    current_angle: f64,
+    setpoint_angle: f64,
+    min_angle: f64,
+    max_angle: f64,
+    p: f64,
+    i: f64,
+    d: f64,
+    max_integral: f64,
+}
+
+impl ServoMotorState {
+    fn new() -> Self {
+        return ServoMotorState {
+            id: 0,
+            current_angle: 0.0,
+            setpoint_angle: 0.0,
+            min_angle: (-180.0_f64).to_radians(), // Example minimum angle
+            max_angle: 180.0_f64.to_radians(),  // Example maximum angle
+            p: 1.0, // Proportional gain
+            i: 0.0, // Integral gain
+            d: 0.0, // Derivative gain
+            max_integral: 10.0, // Maximum integral term to prevent windup
+        };
+    }
+    
+}
+
+#[derive(Clone)]
+struct MotorState {
+    id: u32,
+    current_thrust: f64,
+    setpoint_thrust: f64,
+    max_thrust: f64,
+    p: f64,
+    i: f64,
+    d: f64,
+    max_integral: f64,
+}
+
+impl MotorState {
+    fn new() -> Self {
+        return MotorState {
+            id: 0,
+            current_thrust: 0.0,
+            setpoint_thrust: 0.0,
+            max_thrust: 100.0,  // Example maximum thrust
+            p: 1.0, // Proportional gain
+            i: 0.0, // Integral gain
+            d: 0.0, // Derivative gain
+            max_integral: 100.0, // Maximum integral term to prevent windup
+        };
+    }
+
+}
+
+struct OsprAiState {
+    joints_angles: Vec<ServoMotorState>,
+    thrusters: Vec<MotorState>,
+    linear_acceleration: [f64; 3],
+    angular_velocity: [f64; 3],
+    latitude: f64,
+    longitude: f64,
+    altitude: f64,
+    lidar_state: Option<LidarState>,
+}
+
+impl OsprAiState {
+    /*fn new(bool: contains_lidar, u8: n_thrusters, u) -> Self {
+        return OsprAiState {
+            joints_angles: vec![(0.0, 0.0); 2], // Assuming 2 controllable joints (left and right arms)
+            linear_acceleration: [0.0; 3],
+            angular_velocity: [0.0; 3],
+            latitude: 0.0,
+            longitude: 0.0,
+            altitude: 0.0,
+            lidar_state: None,
+        };
+    }*/
+
+    fn new_with_lidar() -> Self {
+        let mut state= OsprAiState {
+            joints_angles: vec![],
+            thrusters: vec![], // Assuming 2 thrusters
+            linear_acceleration: [0.0; 3],
+            angular_velocity: [0.0; 3],
+            latitude: 0.0,
+            longitude: 0.0,
+            altitude: 0.0,
+            lidar_state: Some(LidarState {
+                min_vertical_angle: 0.0,
+                vertical_resolution: 0.0,
+                min_horizontal_angle: 0.0,
+                horizontal_resolution: 0.0,
+                measurements: vec![],
+            }),
+        };
+        for thruster in &mut state.thrusters {
+            thruster.max_thrust = 1200.0;
+        }
+        return state;
+    }
+    
+}
+
+fn main() {
+    let osprai_state= Arc::new(Mutex::new(OsprAiState::new_with_lidar()));
+    if let Some(mut node)=  Node::new() {
+        let osprai_state_imu = Arc::clone(&osprai_state);
+        let _= node.subscribe("osprai/imu", move |msg : IMU| {
+            if let Ok(mut state) = osprai_state_imu.lock() {
+                state.linear_acceleration = [msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z];
+                state.angular_velocity = [msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z];
+                println!("Attitude: {:?}", msg.orientation);
+                
+            }
+        });
+        let osprai_state_gps = Arc::clone(&osprai_state);
+        let _= node.subscribe("osprai/gps", move |msg : NavSat| {
+            if let Ok(mut state) = osprai_state_gps.lock() {
+                state.latitude = msg.latitude_deg;
+                state.longitude = msg.longitude_deg;
+                state.altitude = msg.altitude;
+            }
+        });
+        let osprai_state_lidar = Arc::clone(&osprai_state);
+        let _= node.subscribe("osprai/lidar", move |msg: LaserScan| {
+            if let Ok(mut state) = osprai_state_lidar.lock() && let Some(lidar_state) = &mut state.lidar_state {
+                lidar_state.min_vertical_angle = msg.vertical_angle_min;
+                lidar_state.vertical_resolution= msg.vertical_angle_step;
+                lidar_state.min_horizontal_angle = msg.vertical_angle_step;
+                lidar_state.horizontal_resolution = msg.angle_min;
+                lidar_state.horizontal_resolution= msg.angle_step;
+                lidar_state.measurements= msg.ranges.clone();
+                //lidar_state.measurements = msg.measurements.clone();
+            
+            }
+        });
+        /*let _= node.subscribe("osprai/lidar/points", move |msg: PointCloudPacked| {
+            println!("Received LIDAR message: {:?}", msg);
+        });*/
+        let osprai_state_joint = Arc::clone(&osprai_state);
+        let _= node.subscribe("/osprai/joint_state", move |msg: Model| {
+            if let Ok(mut state) = osprai_state_joint.lock(){
+                for i in 0..msg.joint.len() {
+                    let j= &msg.joint[i];
+                    if let Some(axis1) = j.axis1.clone().into_option() {
+                        if j.name.contains("spin") {
+                            let mut is_new= true;
+                            for thruster in &mut state.thrusters {
+                                if thruster.id == j.id {
+                                    is_new= false;
+                                    thruster.current_thrust = axis1.velocity;
+                                    break;
+                                }
+                            }
+                            if is_new {
+                                state.thrusters.push(MotorState {
+                                    id: j.id,
+                                    current_thrust: axis1.velocity,
+                                    setpoint_thrust: axis1.velocity,
+                                    max_thrust: 1200.0, // Example maximum thrust
+                                    p: 1.0, // Proportional gain
+                                    i: 0.0, // Integral gain
+                                    d: 0.0, // Derivative gain
+                                    max_integral: 100.0, // Maximum integral term to prevent windup
+                                });
+                            }
+                        } else {
+                            let mut is_new= true;
+                            for joint_state in &mut state.joints_angles {
+                                if joint_state.id == j.id {
+                                    is_new= false;
+                                    joint_state.current_angle = axis1.position;
+                                    break;
+                                }
+                            }
+                            if is_new {
+                                state.joints_angles.push(ServoMotorState {
+                                    id: j.id,
+                                    current_angle: axis1.position,
+                                    setpoint_angle: axis1.position,
+                                    min_angle: axis1.limit_lower,
+                                    max_angle: axis1.limit_upper,
+                                    p: 1.0, // Proportional gain
+                                    i: 0.0, // Integral gain
+                                    d: 0.0, // Derivative gain
+                                    max_integral: 10.0, // Maximum integral term to prevent windup
+                                });
+                            } 
+                        }
+                    } 
+                    
+                }
+            }
+        });
+ 
+        if let Some(mut left_arm) = node.advertise::<Double>("/osprai/command/tilt_left") &&
+                let Some(mut right_arm) = node.advertise::<Double>("/osprai/command/tilt_right") &&
+                let Some(mut thrusters) = node.advertise::<Actuators>("/osprai/command/motor_speed") {
+            let mut start = Instant::now();
+            let dt = Duration::from_millis(300);
+            let osprai_state_cmd = Arc::clone(&osprai_state);
+            let mut increment= -1.0_f64.to_radians();
+            let mut thrust__increment= 10.0;
+            loop {
+                if let Ok(mut state) = osprai_state_cmd.lock() {
+                    let mut need_reverse= false;
+                    let mut idx= 0;
+                    for joint in &mut state.joints_angles {
+                        joint.setpoint_angle += increment;
+                        joint.setpoint_angle = joint.setpoint_angle.clamp(joint.min_angle, joint.max_angle);
+                        if joint.setpoint_angle == joint.max_angle || joint.setpoint_angle == joint.min_angle {
+                            need_reverse = true;
+                        }
+                        println!("Joint {} Current Angle: {:.2}, Setpoint Angle: {:.2}", idx, joint.current_angle, joint.setpoint_angle);
+                        idx += 1;
+                    }
+                    if need_reverse {
+                        increment = -increment; // Reverse direction
+                    }
+                    if state.joints_angles.len() >= 2 {
+                        let mut left_setpoint = Double::new();
+                        left_setpoint.data = state.joints_angles[0].setpoint_angle; // Setpoint angle for left arm
+                        let mut right_setpoint = Double::new();
+                        right_setpoint.data = state.joints_angles[1].setpoint_angle; // Setpoint angle for right arm
+                        let _= left_arm.publish(&left_setpoint);
+                        let _= right_arm.publish(&right_setpoint);
+                    }
+                    idx= 0;
+                    need_reverse= false;
+                    let mut thrusters_command = Actuators::new();
+                    for thruster in &mut state.thrusters {
+                        thruster.setpoint_thrust += thrust__increment;
+                        thruster.setpoint_thrust = thruster.setpoint_thrust.clamp(0.0, thruster.max_thrust);
+                        if thruster.setpoint_thrust == 0.0 || thruster.setpoint_thrust == thruster.max_thrust {
+                            need_reverse = true;
+                        }
+                        thrusters_command.velocity.push(thruster.setpoint_thrust); 
+                        println!("Thruster {} Current Thrust: {:.2}, Setpoint Thrust: {:.2}", idx, thruster.current_thrust, thruster.setpoint_thrust);
+                        idx+=1;
+                    }
+                    if need_reverse {
+                        thrust__increment = -thrust__increment; // Reverse direction
+                    }
+                    let _= thrusters.publish(&thrusters_command);
+                }
+                let elapsed = start.elapsed();
+                if elapsed < dt {
+                    //println!("Sleep for {:?}", dt - elapsed);
+                    sleep(dt - elapsed);
+                    start= start.add(dt); // Reset start time for the next iteration
+                }
+            }
+        }
+    }
+        //ret= node.subscribe("osprai/tilt_right", callback);
+        
+}
+
